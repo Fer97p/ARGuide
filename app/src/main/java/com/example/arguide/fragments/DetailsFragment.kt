4b0028692e116d169be79ef46c3162abcd885e3f
Fer <fernando.pelaez@alumnos.uva.es>
2020-08-12 14:07:28 +0200

    Fragment de detalles añadido

---
+++ app/src/main/java/com/example/arguide/fragments/DetailsFragment.kt
@@ -0,0 +1,58 @@
+package com.example.arguide.fragments
+
+import android.content.Intent
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
+
+import com.example.arguide.R
+import com.example.arguide.main.IntermediateActivity
+import java.lang.ref.PhantomReference
+
+class DetailsFragment : Fragment() {
+
+    private var reference: String = ""
+    private lateinit var textContainer : TextView
+    private lateinit var button: Button
+    private lateinit var viewModel: DetailsViewModel
+    val args: DetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.details_fragment, container, false)
+
+        //Toast.makeText(activity, "Parámetro transferido: ${inputText}", Toast.LENGTH_LONG).show()
+
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
+        val reference = args.placeReference
+        textContainer = requireView().findViewById(R.id.text_container)
+        textContainer.text = reference
+        button = requireView().findViewById(R.id.goToCamera)
+        button.setOnClickListener{
+            val intent = Intent(activity, IntermediateActivity::class.java)
+            startActivity(intent)
+        }
+        button = requireView().findViewById(R.id.button_check)
+        button.setOnClickListener{
+            Toast.makeText(activity, "Parámetro transferido: ${reference}", Toast.LENGTH_LONG).show()
+        }
+
+
+
+    }
+
+}
